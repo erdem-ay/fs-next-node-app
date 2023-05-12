22d2aad687f42c8b57c084c9e3cfa56f6f0b6381
Idris AY <97283155+idrisay@users.noreply.github.com>
2023-05-12 21:43:01 +0200

    feat: add register

--- server/index.js
+++ server/index.js
@@ -3,17 +3,19 @@ const app = express();
 const mongoose = require("./db");
 const configs = require("./configs");
 const { setCorsOptions } = require("./utils");
+const router = require("./routes");
 
 mongoose.connection.once("open", function () {});
 
 app.use(setCorsOptions());
+app.use(express.json());
 
 const port = configs.backendPort;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
 });
-
+app.use("/", router);
 app.listen(port, () => {
   console.log(`Listening http://localhost:${port}/`);
 });
